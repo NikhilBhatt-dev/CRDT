ab3d13a1a00f711e4ab354f416bb22f86ef5bf17
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-04-02 23:19:15 +0530

    workinng on monacobinding

--- frontend/vite-project/src/app/App.jsx
+++ frontend/vite-project/src/app/App.jsx
@@ -1,28 +1,50 @@
 import './App.css'
-import {Editor} from '@monaco-editor/react'
+import { Editor } from '@monaco-editor/react'
+import { MonacoBinding } from "y-monaco"
+import { useRef, useMemo } from 'react'
+import * as Y from 'yjs'
+import { SocketIOProvider } from "y-socket.io"
 
 function App() {
 
-  return(
-    <main className="h-screen w-full  bg-gray-900 flex gap-4 p-4">
-      <aside className='h-full w-1/4 bg-amber-50 rounded-lg'>
+  const editorRef = useRef(null)
+  const ydoc = useMemo(() => new Y.Doc(), [])
+  const yText = useMemo(() => ydoc.getText("monaco"), [ydoc])
 
+  const handleMount = (editor) => {
+    editorRef.current = editor
 
-      </aside>
-      <section className='w-3/4 bg-neutral-800 rounded-lg overflow-hidden '>
+    const provider = new SocketIOProvider(
+      'http://localhost:3000',
+      'monaco',
+      ydoc,{
+      autoConnect: true,
 
-    <Editor
-    height="100%"
-    defaultLanguage='javascript'
-    defaultValue='//some content'
-    theme='vs-dark' />
+  })
+
+    const binding = new MonacoBinding(
+      yText,
+      editorRef.current.getModel(),
+      new Set([editorRef.current]),
+      provider.awareness
+    )
+  }
+
+  return (
+    <main className="h-screen w-full bg-gray-900 flex gap-4 p-4">
+      <aside className='h-full w-1/4 bg-amber-50 rounded-lg'></aside>
+
+      <section className='w-3/4 bg-neutral-800 rounded-lg overflow-hidden'>
+        <Editor
+          height="100%"
+          defaultLanguage='javascript'
+          defaultValue='//some content'
+          theme='vs-dark'
+          onMount={handleMount}
+        />
       </section>
-      
-
     </main>
   )
-
 }
-
 
 export default App
