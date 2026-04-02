2142f4ca428b75c01a9a63197650c681e1d66b0b
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-04-02 22:53:20 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+import express from 'express'
+
+import { Server } from 'socket.io'
+import { createServer } from 'http'
+import {YSocketIO} from "y-socket.io/dist/server"
+
+
+
+const app = express()
+const httpServer = createServer(app)
+
+
+const io =  new Server(httpServer, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+})
+
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message:"hello",
+        success:true
+})
+})
+
+
+app.get('/health', (req,res) => {
+    res.status(200).json({
+        message:"ok",
+        success:true
+    })
+})
+
+
+httpServer.listen(3000, () => {
+    console.log('server is running on port 3000')
+})
